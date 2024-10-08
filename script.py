c96f2646ce447c2ee5d466c54554846dcedebbf5
etud user <etud@localhost>
2024-10-08 15:39:58 +0200

    script finalisé et ajout de commentaires

---
+++ script.py
@@ -0,0 +1,45 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import json
+
+#initialisation d'un nouveau graphe
+G = nx.DiGraph()
+
+#lecture du fichier json
+fichier = open("graphe.json", "r")
+taches = json.load(fichier)
+
+num = ""
+predecesseur = ""
+duree = ""
+
+#pour chaque tâche du projet
+for tache in range(len(taches)):
+    #on récupère le numéro et la durée de la tâche
+    num = taches[tache]["num"]
+    duree = taches[tache]["duree"]
+
+    #si plusieurs prédecesseurs
+    if (len(taches[tache]["predecesseur"]) > 1):
+        predecesseurs = taches[tache]["predecesseur"]
+        for i in range(len(predecesseurs)):
+            #ajout d'un segment au graphe
+            G.add_edge(predecesseurs[i], num, weight=duree)
+
+    #si pas de prédecesseur (cas de la première tâche)
+    elif (taches[tache]["predecesseur"][0] == 0):
+        continue
+
+    else:
+        predecesseur = taches[tache]["predecesseur"][0]
+        #ajout d'un segment au graphe
+        G.add_edge(predecesseur, num, weight=duree)
+    
+
+#dessin du graphe
+    #pos = nx.shell_layout(G)
+    #nx.draw(G, pos, with_labels=True, arrows=True, node_size=700, node_color="skyblue")
+    #plt.show()
+
+
+fichier.close()
