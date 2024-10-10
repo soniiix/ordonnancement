74d8c072867a4856eebaa3f07f5a4e7edd8ea89c
etud user <etud@localhost>
2024-10-10 11:42:09 +0200

    commentaires retirés pour le dessin du graphe

--- script.py
+++ script.py
@@ -39,9 +39,9 @@ for tache in range(len(taches)):
 G.add_edge(len(taches)-1, len(taches))
 
 #dessin du graphe
-#pos = nx.shell_layout(G)
-#nx.draw(G, pos, with_labels=True, arrows=True, node_size=700, node_color="skyblue")
-#plt.show()
+pos = nx.shell_layout(G)
+nx.draw(G, pos, with_labels=True, arrows=True, node_size=700, node_color="skyblue")
+plt.show()
 
 
 fichier.close()
