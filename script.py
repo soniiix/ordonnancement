ac43af59a9460c1dcfe392fe93ce510cfb0b7c41
soniiix <quent.couzinet@gmail.com>
2024-10-25 10:44:15 +0200

    optimisation du code

--- script.py
+++ script.py
@@ -2,7 +2,8 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import json
 
-def chercherDuree(taches, predecesseur):
+#fonction pour chercher la durée de la tâche précédente
+def chercher_duree(taches, predecesseur):
     for tache in taches :
         if tache["num"] == predecesseur:
             return tache['duree']
@@ -11,34 +12,34 @@ def chercherDuree(taches, predecesseur):
 G = nx.DiGraph()
 
 #lecture du fichier json
-fichier = open("graphe.json", "r")
+fichier = open("data.json", "r")
 taches = json.load(fichier)
 
 #pour chaque tâche du projet
-for tache in range(len(taches)):
+for tache in taches:
     #on récupère le numéro de la tâche
-    num = taches[tache]["num"]
+    num = tache["num"]
 
-    #si plusieurs prédecesseurs
-    if (len(taches[tache]["predecesseur"]) > 1):
-        predecesseurs = taches[tache]["predecesseur"]
-        for i in range(len(predecesseurs)):
-            duree = chercherDuree(taches, predecesseurs[i])
+    #on évite le cas de la première tâche (pas de prédécesseur)
+    if tache["predecesseur"][0] != 0:
+
+        #si plusieurs prédecesseurs
+        if (len(tache["predecesseur"]) > 1):
+            predecesseurs = tache["predecesseur"]
+            for predecesseur in predecesseurs:
+                duree = chercher_duree(taches, predecesseur)
+                #ajout d'un segment au graphe
+                G.add_edge(predecesseur, num, weight=duree)
+
+        #si un seul prédécesseur
+        else:
+            predecesseur = tache["predecesseur"][0]
+            duree = chercher_duree(taches, predecesseur)
             #ajout d'un segment au graphe
-            G.add_edge(predecesseurs[i], num, weight=duree)
+            G.add_edge(predecesseur, num, weight=duree)
 
-    #si pas de prédecesseur (cas de la première tâche)
-    elif (taches[tache]["predecesseur"][0] == 0):
-        continue
-
-    else:
-        predecesseur = taches[tache]["predecesseur"][0]
-        duree = chercherDuree(taches, predecesseur)
-        #ajout d'un segment au graphe
-        G.add_edge(predecesseur, num, weight=duree)
-
-#ajout de τ
-duree = chercherDuree(taches, len(taches))
+#ajout de τ, la tâche finale
+duree = chercher_duree(taches, len(taches))
 G.add_edge(len(taches), len(taches)+1, weight=duree)
 
 
@@ -47,11 +48,17 @@ for layer, nodes in enumerate(nx.topological_generations(G)):
     for node in nodes:
         G.nodes[node]["layer"] = layer
 pos = nx.multipartite_layout(G, subset_key="layer")
+
 #dessin du graphe
 nx.draw(G, pos, with_labels=True, arrows=True, node_size=700, node_color="skyblue")
+
 #ajout de l'affichage de la durée des tâches
 edge_labels = nx.get_edge_attributes(G, "weight")
 nx.draw_networkx_edge_labels(G, pos, edge_labels)
+
+
+#sauvegarde du graphe
+plt.savefig("graphe.png")
 
 plt.show()
 
